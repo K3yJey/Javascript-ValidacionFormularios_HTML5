b854181bd66dc3f9926ff43498a935508734651a
K3yJey <kevin_madrid10@hotmail.es>
2023-04-14 11:40:05 -0500

    fix: :fire: Se cambió la estructura del proyecto

--- script/validaciones.js
+++ script/validaciones.js
@@ -4,7 +4,6 @@ export function valida(input) {
     if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
     }
-
     if (input.validity.valid) {
         input.parentElement.classList.remove("input-container--invalid");
         input.parentElement.querySelector(".input-message-error").innerHTML = "";
@@ -69,7 +68,6 @@ function mostrarMensajeDeError(tipoDeInput, input) {
             mensaje = mensajesDeError[tipoDeInput][error];
         }
     });
-
     return mensaje;
 }
 
@@ -80,18 +78,15 @@ function validarNacimiento(input) {
     if (!mayorDeEdad(fechaCliente)) {
         mensaje = "Debes tener al menos 18 años de edad";
     }
-
     input.setCustomValidity(mensaje);
 }
 
 function mayorDeEdad(fecha) {
     const fechaActual = new Date();
-
     const diferenciaFechas = new Date(
         fecha.getUTCFullYear() + 18,
         fecha.getUTCMonth(),
         fecha.getUTCDate()
     );
-
     return diferenciaFechas <= fechaActual;
 }
